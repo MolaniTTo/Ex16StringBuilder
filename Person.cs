05a3f71692237e4e35285ef6c9803d98801d9e10
arnauitb <arnau.molano.7e7@itb.cat>
2024-03-11 12:18:45 +0000

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+
+
+namespace Ex16StringBuilder
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public override string ToString()
+        {
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("NOM: " + Name);
+
+            sb.Append('\n');
+
+            sb.Append("EDAT: " + Age);
+
+            return sb.ToString();
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Ex16StringBuilder;
+using System.Text;
+
+namespace StringBuilderExercici
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Person arnau = new Person("Arnau", 18);
+
+            Console.WriteLine(arnau);
+
+        }
+    }
+}
